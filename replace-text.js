7cd7b28475bc9ac055964827430c1d4a2dd95512
dongfubao <2804306132@qq.com>
2020-01-14 13:34:12 +0800

    修改生命周期

--- replace-text.js
+++ replace-text.js
@@ -2,7 +2,7 @@
 // 字段配置
 import { fieldInfo } from "./replace-text.field.js"
 
-var _self;
+let _self;
 export default {
     // 导入的组件
     components: {
@@ -14,23 +14,28 @@ export default {
         }
     },
     // 初始化加载
-    beforeCreate() {
-        console.group('replace-text组件 beforeCreate 状态=====》');
-    },
-    onLoad(option) {
-        _self = this;
-        console.log('onLoad 状态=====》');
-    },
-    
-    onShow() {
-        console.log('onShow 状态=====》');
-    },
-    onReady() {
-        console.log('onReady 状态====》');
-    },
-    onUnload() {
-        console.groupEnd('onUnload 状态=====》');
-    },
+	beforeCreate() {
+		console.group('replace-text组件 beforeCreate');
+	},
+	onLoad(option) {
+		_self = this;
+		console.log('replace-text组件 onLoad');
+	},
+
+	onShow() {
+		console.group('replace-text组件 onShow');
+	},
+	onReady() {
+		console.log('replace-text组件 onReady 状态');
+	},
+	onUnload() {
+		console.log('replace-text组件 onUnload');
+		console.groupEnd()
+	},
+	onHide() {
+		console.log('replace-text组件 onHide');
+		console.groupEnd()
+	},
     // 下拉刷新
     onPullDownRefresh:() => {
     
